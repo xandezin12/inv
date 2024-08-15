294f7db7c33fd30ce42d05817a233b1f94cc76aa
xandezin12 <ceodosexo12@gmail.com>
2024-08-15 12:53:21 -0300

    ok

---
+++ invasaoaoplanalto/hentai.js
@@ -0,0 +1,24 @@
+
+{
+    "nome": "mountain",
+    "tipo": "terreno",
+    "subtipo": "Montanha",
+    "custo_mana": "0",
+    "custo_mana_total": "0",
+    "texto": " nenhum",
+    "forca": "0",
+    "resistencia": "0",
+    "lealdade": "0",
+    "raridade": "normal",
+    "colecao": "The Brothers' War",
+    "num_serie": "275",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {
+        "nome": "nenhum",
+        "descricao": "0",
+        "tipo": "0"
+      }
+    
+   ]
+ }
