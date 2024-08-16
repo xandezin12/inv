b7ad475154949d13978749ecfed11428ec53efd6
xandezin12 <161892273+xandezin12@users.noreply.github.com>
2024-08-16 15:43:50 -0300

    dd

--- invasaoaoplanalto/hentai.js
+++ invasaoaoplanalto/hentai.js
@@ -7,6 +7,7 @@ const cartas = [
     "raridade": "comum",
     "colecao": "The Brothers' War",
     "num_serie": "275",
+    "quantidade": "8",
     "imagem_url": "URL da Imagem",
     "habilidades": []
   },
@@ -19,6 +20,7 @@ const cartas = [
     "raridade": "comum",
     "colecao": "The Brothers' War",
     "num_serie": "277",
+    "quantidade": "8",
     "imagem_url": "URL da Imagem",
     "habilidades": []
   },
@@ -149,11 +151,11 @@ const cartas = [
   },
   {
     "nome": "Bushwhack",
-    "tipo": "Artifact Creature — Dragon",
-    "custo_mana": "8",
-    "valor de mana": "8",
-    "forca": "5",
-    "resistencia": "5",
+    "tipo": "Sorcery",
+    "custo_mana": "x",
+    "valor de mana": "1",
+    "forca": "x",
+    "resistencia": "x",
     "raridade": "incomum",
     "colecao": "The Brothers' War",
     "num_serie": "174",
@@ -168,19 +170,161 @@ const cartas = [
   {
     "nome": "Epic Confrontation",
     "tipo": "sorcery",
-    "custo_mana": "8",
-    "valor de mana": "8",
-    "forca": "5",
-    "resistencia": "5",
-    "raridade": "incomum",
+    "custo_mana": "1",
+    "valor de mana": "2",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
     "colecao": "The Brothers' War",
-    "num_serie": "159",
+    "num_serie": "176",
     "imagem_url": "URL da Imagem",
     "habilidades": [
-      {"nome": "Voar (Flying), Habilidade de Pump, Habilidade de Pump"
+      {"nome": "Target creature you control gets +1/+2 until end of turn. It fights target creature you don't control. (Each deals damage equal to its power to the other.)"
+ }
+    ]
+  },
+
+  {
+    "nome": "Mishra's Onslaught",
+    "tipo": "Instant",
+    "custo_mana": "3",
+    "valor de mana": "4",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "143",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "• Create two 1/1 colorless Soldier artifact creature tokens. Creatures you control get +2/+0 until end of turn."
   
       }
     ]
   },
+  {
+    "nome": "Artificer's Dragon",
+    "tipo": "Artifact Creature — Dragon",
+    "custo_mana": "3",
+    "valor de mana": "4",
+    "forca": "4",
+    "resistencia": "4",
+    "raridade": "raro",
+    "colecao": "The Brothers' War",
+    "num_serie": "291",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Flying, Unearth"
+  
+      }
+    ]
+  },
+  {
+    "nome": "Mishra's Juggernaut",
+    "tipo": "Artifact Creature — Juggernaut",
+    "custo_mana": "5",
+    "valor de mana": "5",
+    "forca": "5",
+    "resistencia": "3",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "161",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Trample, Unearth"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Unleash Shell",
+    "tipo": "Instant",
+    "custo_mana": "3",
+    "valor de mana": "5",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "155",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Unleash Shell deals 5 damage to target creature or planeswalker and 2 damage to that permanent's controller."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Cradle Clearcutter",
+    "tipo": "Artifact Creature — Golem",
+    "custo_mana": "6",
+    "valor de mana": "6",
+    "forca": "3",
+    "resistencia": "6",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "198",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Add an amount of Green equal to Cradle Clearcutter's power, Prototype"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Skitterbeam Battalion",
+    "tipo": "Artifact Creature — Construc",
+    "custo_mana": "9",
+    "valor de mana": "9",
+    "forca": "4",
+    "resistencia": "4",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "165",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Trample, haste, Prototype"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Scrapwork Mutt",
+    "tipo": "Artifact Creature — Dog",
+    "custo_mana": "2",
+    "valor de mana": "2",
+    "forca": "2",
+    "resistencia": "1",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "164",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "When Scrapwork Mutt enters, you may discard a card. If you do, draw a card. Unearth"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Scrapwork Mutt",
+    "tipo": "Artifact Creature — Wurm",
+    "custo_mana": "7",
+    "valor de mana": "7",
+    "forca": "4",
+    "resistencia": "6",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "200",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Whenever Iron-Craw Crusher attacks, target attacking creature gets +X/+0 until end of turn, where X is Iron-Craw Crusher's power. Prototype"
+  
+      }
+    ]
+  },
+
 
 ]
