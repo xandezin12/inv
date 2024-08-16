9f651450ba8aaaf2088ee0a62cbf16ba8ba14790
xandezin12 <161892273+xandezin12@users.noreply.github.com>
2024-08-16 13:49:15 -0300

    ok

--- invasaoaoplanalto/hentai.js
+++ invasaoaoplanalto/hentai.js
@@ -1,24 +1,156 @@
-
-{
-    "nome": "mountain",
-    "tipo": "terreno",
-    "subtipo": "Montanha",
-    "custo_mana": "0",
-    "custo_mana_total": "0",
-    "texto": " nenhum",
+const cartas = [
+  {
+    "nome": "Mountain",
+    "tipo": "Terreno",
     "forca": "0",
     "resistencia": "0",
-    "lealdade": "0",
-    "raridade": "normal",
+    "raridade": "comum",
     "colecao": "The Brothers' War",
     "num_serie": "275",
     "imagem_url": "URL da Imagem",
+    "habilidades": []
+  },
+  {
+    "nome": "Batalhão Skitterbeam",
+    "tipo": "Terreno",
+    "valor de mana": "9",
+    "custo_mana": "9",
+    "forca": "4",
+    "resistencia": "4",
+    "raridade": "Mitico Raro",
+    "colecao": "The Brothers' War",
+    "num_serie": "165",
+    "imagem_url": "URL da Imagem",
     "habilidades": [
-      {
-        "nome": "nenhum",
-        "descricao": "0",
-        "tipo": "0"
+      {"nome": "Atropelar, pressa, Protótipo"
+
       }
-    
-   ]
- }
+    ]
+  },
+  {
+    "nome": "Amálgama Rootwire",
+    "tipo": "Criatura Artefato - Golem",
+    "valor de mana": "5",
+    "custo_mana": "5",
+    "forca": "5",
+    "resistencia": "5",
+    "raridade": "Mitico Raro",
+    "colecao": "The Brothers' War",
+    "num_serie": "203",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Sacrifique Rootwire Amalgam, Protótipo"
+        
+      }
+    ]
+  },
+
+  
+  {
+    "nome": "Mishra, Excavation Prodigy",
+    "tipo": "Legendary Creature — Human Artificer",
+    "custo_mana": "2",
+    "valor de mana" : "3",
+    "forca": "2",
+    "resistencia": "1",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "140",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Discard a card"
+        
+      }
+    ]
+  },
+  {
+    "nome": "Sarinth Steelseeker",
+    "tipo": "Creature — Human Artificer Scout",
+    "custo_mana": "1",
+    "valor de mana": "2",
+    "forca": "1",
+    "resistencia": "2",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "189",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Quando um ou mais artefatos entram no campo de batalha sob o seu controle, você pode olhar a carta do topo do seu grimório. Se for uma carta de terreno, você pode revelá-la e colocá-la em sua mão. Se você não colocar a carta na sua mão, pode colocá-la no seu cemitério."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Fallaji Dragon Engine",
+    "tipo": "Artifact Creature — Dragon",
+    "custo_mana": "8",
+    "valor de mana": "8",
+    "forca": "5",
+    "resistencia": "5",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "159",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Voar (Flying), Habilidade de Pump, Habilidade de Pump"
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Tawnos's Tinkering",
+    "tipo": "Instant",
+    "custo_mana": "3",
+    "valor de mana": "4",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "191",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Coloque dois marcadores +1/+1 em um artefato, criatura ou terreno alvo que você controla. Desvire aquela permanente. Se ela não for uma criatura, ela se torna uma criatura 0/0 além dos seus outros tipos."
+  
+      }
+    ]
+  },
+
+  {
+    "nome": "Shoot Down",
+    "tipo": "Sorcery",
+    "custo_mana": "3",
+    "valor de mana": "4",
+    "forca": "x",
+    "resistencia": "x",
+    "raridade": "comum",
+    "colecao": "The Brothers' War",
+    "num_serie": "190",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Exile o artefato, encantamento ou criatura com voar alvo."
+  
+      }
+    ]
+  },
+  {
+    "nome": "Bushwhack",
+    "tipo": "Artifact Creature — Dragon",
+    "custo_mana": "8",
+    "valor de mana": "8",
+    "forca": "5",
+    "resistencia": "5",
+    "raridade": "incomum",
+    "colecao": "The Brothers' War",
+    "num_serie": "174",
+    "imagem_url": "URL da Imagem",
+    "habilidades": [
+      {"nome": "Procure em seu grimório um card de terreno básico, revele-o, coloque-o em sua mão e depois embaralhe, A criatura alvo que você controla luta contra a criatura alvo que você não controla. (Cada uma causa dano igual ao seu poder à outra.)"
+  
+      }
+    ]
+  },
+
+
+]
